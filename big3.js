a46928550c23bbb549ee6d36e3e33c0a59def5da
pavani1280 <pavani.8021@gmail.com>
2026-03-13 16:51:15 +0530

    Add files via upload

---
+++ big3.js
@@ -0,0 +1,2 @@
+let a=15, b=100, c=20
+a>b&&a>c?console.log("a is max",a):b>c?console.log("b is max",b):console.log("c is max",c);
---
+++ sumByArray.js
@@ -0,0 +1,7 @@
+let q=[10,71,11,128];
+let sum=0;
+for(let i=0;i<q.length;i++){
+    sum+=q[i];
+    
+}
+console.log("sum is ",sum);
