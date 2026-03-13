2cc368277ab9aeaf3fcc7f2e9b764b29f8a8a34e
pavani1280 <pavani.8021@gmail.com>
2026-03-13 20:56:06 +0530

    Add files via upload

---
+++ sunteck/week--3/productAPI.js
@@ -0,0 +1,52 @@
+//create product APIwith below operation
+//crete a new product({product Id,name,brand,price})
+//read all products
+//read all product by brand
+//update the product 
+//delete the product by id
+import exp from 'express'
+export const productApp =exp.Router()
+let cust =[]
+
+productApp.get('/products',(req,res)=>{
+    res.json({message:"getting",payload: cust});
+})
+
+
+productApp.post('/products',(req,res)=>{
+    
+   const newUser=req.body
+   cust.push(newUser)
+   res.json({message:"user created"})
+
+})
+productApp.put('/products',(req,res)=>{
+    let modifieduser=req.body
+    let index = cust.findIndex(userObj=>userObj.productId===modifieduser.productId)
+    if (index ===-1){
+    return res.json({message:"not found"})
+    }
+    cust.splice(index,1,modifieduser)
+
+    res.json({message:"updated successfully"})
+
+})
+
+productApp.delete('/products/:id',(req,res)=>{
+   
+    //get id of user from url parameter
+    let idOfUrl = Number(req.params.id)
+    //find the index of the user
+    let index= cust.findIndex(userObj => userObj.productId=== idOfUrl)
+    //if user not found
+    if(index === -1){ 
+        return res.json({message:"user not found"})
+    }
+    //delete user by index
+    cust.splice(index,1)
+    //send re to client
+    res.json({message:"user DELETED"})
+})
+
+
+
