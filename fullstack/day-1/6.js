18ed8591b3a58c6c6967aa0ddc52f1abdef943d9
pavani1280 <pavani.8021@gmail.com>
2026-03-16 19:20:47 +0530

    Add files via upload

--- fullstack/day-1/6.js
+++ fullstack/day-1/6.js
@@ -1,1 +1,9 @@
-
+function arrsu(arr){
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+let a = [10,20,30,40];
+console.log("sum is ",arrsu(a));
---
+++ fullstack/day-1/minInArray.js
@@ -0,0 +1,8 @@
+let q=[90,8,65,98];
+let min=q[0];
+for(let i=0;i<q.length;i++){
+   
+    if(q[i]<min)
+        min=q[i];
+}
+console.log("min is ",min);
