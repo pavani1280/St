54d5e7b26a61b511df2cc58cc8d5699b20b2ddc6
pavani1280 <pavani.8021@gmail.com>
2026-05-18 15:20:30 +0530

    Update CORS settings and clean up server.js

--- fullstack/week-7/backend/server.js
+++ fullstack/week-7/backend/server.js
@@ -13,7 +13,7 @@ const app=exp()
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173","https://capstone-project-theta-eight.vercel.app/"], 
     credentials: true, 
   })
 );
